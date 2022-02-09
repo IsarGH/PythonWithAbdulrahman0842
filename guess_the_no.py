c7d226b6b86ad484d69c4c25d7c76393e6cb4d70
Abdulrahman <94384418+abdulrahman0842@users.noreply.github.com>
2022-02-09 14:30:22 +0530

    Add files via upload

---
+++ guess_the_no.py
@@ -0,0 +1,22 @@
+                                     # Guess the Number
+n = int(18)
+iterations = int(0)
+guess =int(10)
+while(True):
+    iterations = iterations+1
+    guess = guess-1
+    print(guess ,"Guesses Left")
+    if iterations==10:
+        print("Game Over!")
+        break
+    inp = int(input("Enter No."))
+    if inp>18:
+        print("Too Large")
+        continue
+    elif inp<18:
+        print("Too Short")
+        continue
+    else:
+        print("Congratulations! You Guess the correct No.")
+        print(10-guess, "Guesses you took.")
+        break
